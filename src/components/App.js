873a3f669203d5ff9e4d538189adbef06da66001
patchdpineapple <seanpatrick.penaranda@gmail.com>
2021-03-24 17:04:52 +0800

    made nav bar  ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//import components
+import Navlinks from "./Navlinks";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <h1>Project Start</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navlinks isLoggedIn={isLoggedIn} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navlinks.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Navlinks.css";
+import { Link } from "react-router-dom";
+
+function LoggedIn() {
+  return (
+    <ul class="nav-links">
+          <li><Link to="/profile" className="link btn btn-link btn-username">Username</Link></li>
+          <li><Link to="/" className="link btn btn-link btn-logout">Logout</Link></li>
+        </ul>
+  );
+}
+
+function LoggedOut() {
+  return (
+    <ul class="nav-links">
+          <li><Link to="/login" className="link btn btn-link btn-login">Login</Link></li>
+          <li><Link to="/signup" className="link btn btn-link btn-signup">Sign Up</Link></li>
+        </ul>
+  );
+}
+
+function Navlinks({isLoggedIn}) {
+  return (
+  <div className="Navlinks">
+    <h1 class="nav-logo"><Link to="/" className="link">The Hub</Link></h1>
+        {isLoggedIn ? <LoggedIn /> : <LoggedOut />}
+  </div>
+  );
+}
+
+export default Navlinks;
